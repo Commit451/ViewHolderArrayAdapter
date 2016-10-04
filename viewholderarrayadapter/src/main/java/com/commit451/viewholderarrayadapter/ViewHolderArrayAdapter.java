868c2083751085875e1dbe9a6d20bd3144c70acc
John Carlson <jawnnypoo@gmail.com>
2016-10-04 00:04:08 -0500

    Add more docs

--- viewholderarrayadapter/src/main/java/com/commit451/viewholderarrayadapter/ViewHolderArrayAdapter.java
+++ viewholderarrayadapter/src/main/java/com/commit451/viewholderarrayadapter/ViewHolderArrayAdapter.java
@@ -13,10 +13,41 @@ import java.util.List;
  * Uses the magic of {@link android.support.v7.widget.RecyclerView.ViewHolder}s with
  * {@link ArrayAdapter} to make great Spinner adapters
  */
-public abstract class ViewHolderArrayAdapter<T, H extends RecyclerView.ViewHolder> extends ArrayAdapter<T> {
+public abstract class ViewHolderArrayAdapter<T, VH extends RecyclerView.ViewHolder> extends ArrayAdapter<T> {
 
-    protected abstract H onCreateViewHolder(ViewGroup parent);
-    protected abstract void onBindViewHolder(H holder, T item, int position);
+    /**
+     * Called when Spinner needs a new {@link RecyclerView.ViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using
+     * {@link #onBindViewHolder(RecyclerView.ViewHolder, Object, int)}. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     *               an adapter position.
+     *
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see android.support.v7.widget.RecyclerView.Adapter#onCreateViewHolder(ViewGroup, int)
+     * @see #onBindViewHolder(RecyclerView.ViewHolder, Object, int)
+     */
+    protected abstract VH onCreateViewHolder(ViewGroup parent);
+
+    /**
+     * Called by Spinner to display the data at the specified position. This method should
+     * update the contents of the {@link RecyclerView.ViewHolder#itemView} to reflect the item at the given
+     * position.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param item The item that should be bound
+     * @param position The position of the item within the adapter's data set.
+     */
+    protected abstract void onBindViewHolder(VH holder, T item, int position);
 
     public ViewHolderArrayAdapter(Context context, List<T> objects) {
         super(context, 0, objects);
@@ -35,12 +66,14 @@ public abstract class ViewHolderArrayAdapter<T, H extends RecyclerView.ViewHolde
 
     private View getTheView(int position, View convertView, ViewGroup parent) {
         T item = getItem(position);
-        H holder;
+        VH holder;
         if (convertView == null) {
             holder = onCreateViewHolder(parent);
             holder.itemView.setTag(R.id.view_holder_array_adapter_id, holder);
         } else {
-            holder = (H) convertView.getTag(R.id.view_holder_array_adapter_id);
+            //no need to worry, we are the only ones setting this tag
+            //noinspection unchecked
+            holder = (VH) convertView.getTag(R.id.view_holder_array_adapter_id);
         }
         onBindViewHolder(holder, item, position);
         return holder.itemView;
